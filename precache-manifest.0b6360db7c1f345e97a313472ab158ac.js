096560045d7972cc59a8c4cd7a197df8a2daffce
unknown <sinisasukovic1234@gmail.com>
2020-01-18 18:56:01 +0100

    Updates

---
+++ precache-manifest.0b6360db7c1f345e97a313472ab158ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088b14615b3767139e9a1dd63af8c045",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "9b51277e710d1005af88",
+    "url": "/todoApp/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "505798baf06a4d7ad44c",
+    "url": "/todoApp/static/css/main.9a3893d1.chunk.css"
+  },
+  {
+    "revision": "9b51277e710d1005af88",
+    "url": "/todoApp/static/js/2.a50053b5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todoApp/static/js/2.a50053b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "505798baf06a4d7ad44c",
+    "url": "/todoApp/static/js/main.30d15183.chunk.js"
+  },
+  {
+    "revision": "ad59bfc624bc8b95e7fd",
+    "url": "/todoApp/static/js/runtime-main.000367e2.js"
+  }
+]);
